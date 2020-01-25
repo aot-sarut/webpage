7ace2832b856a1638412d4f3a4dd8e8146b9446d
sarut <60289607+aot-sarut@users.noreply.github.com>
2020-01-25 22:03:29 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>firstWeb</title>
+</head>
+<body>
+    <h1>Sarut Khakakhapong</h1>
+    <hr noshade>
+    <img src="img/123.jpg"width=250 hight=250>
+    
+</body>
+</html>
